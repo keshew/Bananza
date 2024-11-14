b3ed6fc07ac191f9563a6e59df1c25f35981d072
keshew <artyom.korotckov2017@yandex.ru>
2024-11-14 14:18:10 +0500

    Initial Commit

---
+++ Bananza/BananzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BananzaApp.swift
+//  Bananza
+//
+//  Created by Артём Коротков on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BananzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
